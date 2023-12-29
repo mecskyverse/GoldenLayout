b3d91ad41c530b530af9a23d48e9f9a589546824
Aakash <vermaaakash1000@gmail.com>
2023-12-29 22:47:47 +0530

    App.jsx golden Layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
+//For modular design importing different components and hooks
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 import './App.css'
+import '@annotationhub/react-golden-layout/dist/css/goldenlayout-base.css';
+import '@annotationhub/react-golden-layout/dist/css/themes/goldenlayout-dark-theme.css';
+import { GoldenLayoutComponent } from '@annotationhub/react-golden-layout';
 
+function ComponentA() {
+  return <h2>A</h2>;
+}
+
+function ComponentB() {
+  return <h2>B</h2>;
+}
+
+function ComponentC(props) {
+  return <h2>{props.myText}</h2>;
+}
+
+//Starting point of whole website 
 function App() {
-  const [count, setCount] = useState(0)
+  const [layoutManager, setLayoutManager] = useState(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app-container'>
+      <Navbar />
+        {/* From here the Golden Layout Starts it gets three different componets and arrange it as per the config */}
+      <div style={{ width: '100%', height: '90%' }}>
+        <GoldenLayoutComponent
+          // (Required) Golden Layout Config. (See http://golden-layout.com/docs/Config.html)
+          config={{
+            // you can change the type to arrange it row wise 
+            content: [{
+              type: 'column',
+              content: [{
+                component: ComponentA,
+                title: 'A Component'
+              }, {
+                type: 'row',
+                content: [{
+                  component: ComponentB,
+                  title: 'B Component'
+                }, {
+                  component: () => <ComponentC myText="Component with Props" />,
+                  title: 'C Component'
+                }]
+              }]
+            }]
+          }}
+          // (Optional) Set up auto-resizing. Layout will resize when the window resizes.
+          autoresize={true}
+          // (Optional) (Milliseconds) Debounce resize to prevent excessive re-renders.
+          debounceResize={100}
+          // (Optional) Grab the instance of the GoldenLayout Manager. Gives you full access to GL API.
+          onLayoutReady={setLayoutManager}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
